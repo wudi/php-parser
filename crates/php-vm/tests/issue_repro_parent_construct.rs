3c25f1e5de55e936f3bb276a43314a95dd38c71b
wudi <hi@wudi.io>
2025-12-08 12:18:21 +0800

    feat: add tests for parent constructor calls and self static method calls

---
+++ crates/php-vm/tests/issue_repro_parent_construct.rs
@@ -0,0 +1,107 @@
+use php_vm::vm::engine::VM;
+use php_vm::runtime::context::{EngineContext, RequestContext};
+use php_vm::core::value::Val;
+use php_vm::compiler::emitter::Emitter;
+use std::sync::Arc;
+use std::rc::Rc;
+
+#[test]
+fn test_parent_construct_call() {
+    let src = r#"<?php
+        class Person {
+            public $name;
+            public $age;
+            
+            public function __construct($name, $age) {
+                $this->name = $name;
+                $this->age = $age;
+            }
+        }
+
+        class Employee extends Person {
+            public $employeeId;
+            
+            public function __construct($name, $age, $employeeId) {
+                parent::__construct($name, $age);
+                $this->employeeId = $employeeId;
+            }
+            
+            public function getInfo() {
+                return $this->name . "|" . $this->age . "|" . $this->employeeId;
+            }
+        }
+
+        $employee = new Employee("Bob", 40, "E123");
+        return $employee->getInfo();
+    "#;
+    
+    let context = Arc::new(EngineContext::new());
+    let mut request_context = RequestContext::new(context);
+    
+    let arena = bumpalo::Bump::new();
+    let lexer = php_parser::lexer::Lexer::new(src.as_bytes());
+    let mut parser = php_parser::parser::Parser::new(lexer, &arena);
+    let program = parser.parse_program();
+    
+    if !program.errors.is_empty() {
+        panic!("Parse errors: {:?}", program.errors);
+    }
+
+    let mut emitter = Emitter::new(src.as_bytes(), &mut request_context.interner);
+    let (chunk, _) = emitter.compile(program.statements);
+    
+    let mut vm = VM::new_with_context(request_context);
+    vm.run(Rc::new(chunk)).unwrap();
+    
+    let res_handle = vm.last_return_value.expect("Should return value");
+    let res_val = vm.arena.get(res_handle).value.clone();
+    
+    if let Val::String(s) = res_val {
+        assert_eq!(String::from_utf8_lossy(&s), "Bob|40|E123");
+    } else {
+        panic!("Expected string return value, got {:?}", res_val);
+    }
+}
+
+#[test]
+fn test_self_static_call_to_instance_method() {
+    let src = r#"<?php
+        class A {
+            public function foo() {
+                return "foo";
+            }
+            public function bar() {
+                return self::foo() . static::foo() . A::foo();
+            }
+        }
+        $a = new A();
+        return $a->bar();
+    "#;
+    
+    let context = Arc::new(EngineContext::new());
+    let mut request_context = RequestContext::new(context);
+    
+    let arena = bumpalo::Bump::new();
+    let lexer = php_parser::lexer::Lexer::new(src.as_bytes());
+    let mut parser = php_parser::parser::Parser::new(lexer, &arena);
+    let program = parser.parse_program();
+    
+    if !program.errors.is_empty() {
+        panic!("Parse errors: {:?}", program.errors);
+    }
+
+    let mut emitter = Emitter::new(src.as_bytes(), &mut request_context.interner);
+    let (chunk, _) = emitter.compile(program.statements);
+    
+    let mut vm = VM::new_with_context(request_context);
+    vm.run(Rc::new(chunk)).unwrap();
+    
+    let res_handle = vm.last_return_value.expect("Should return value");
+    let res_val = vm.arena.get(res_handle).value.clone();
+    
+    if let Val::String(s) = res_val {
+        assert_eq!(String::from_utf8_lossy(&s), "foofoofoo");
+    } else {
+        panic!("Expected string return value, got {:?}", res_val);
+    }
+}
